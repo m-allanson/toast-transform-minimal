bc20524afd5388d5fe34e82aa7b10becd8d8632d
m-allanson <michael.allanson@gmail.com>
2021-08-19 23:16:25 +0100

    Add example code, re-run build

--- .tmp/src/pages/index.js
+++ .tmp/src/pages/index.js
@@ -1,3 +1,10 @@
 import { h } from "preact";
-export default ((props)=>h("div", null, h("h1", null, "a marketing landing page"))
-);
+export default ((props)=>{
+    // Start with r = 0
+    let r;
+    // Then assign it another value
+    r = "0x" + props.foo;
+    // Finally log r. It shouldn't be transformed to a literal 0
+    console.log('Logging r, which should not be zero', 0);
+    return h("div", null, h("h1", null, "a marketing landing page"));
+});
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,19 @@
 import { h } from "preact";
 
-export default (props) => (
-  <div>
-    <h1>a marketing landing page</h1>
-  </div>
-);
+export default (props) => {
+
+  // Start with r = 0
+  let r = 0;
+
+  // Then assign it another value
+  r = "0x" + props.foo
+
+  // Finally log r. It shouldn't be transformed to a literal 0
+  console.log('Logging r, which should not be zero', r);
+
+  return (
+    <div>
+      <h1>a marketing landing page</h1>
+    </div>
+  )
+};
